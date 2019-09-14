553fcefce9a6b7e427b56972fa83fd1dbd37e9d5
rodrigobluee <rodrigobluee@gmail.com>
2019-09-14 17:35:01 -0300

    Add files via upload

---
+++ Logic_Test_2.py
@@ -0,0 +1,30 @@
+import json
+from datetime import datetime, timedelta
+
+config = json.loads(open('U1ZQR43RB.json').read())
+
+dicts={}
+
+for line in range(len(config)):
+    start = config[line]['ts']
+    
+    if not dicts.__contains__(config[line]['user']):
+        dicts.update({config[line]['user']:{}})
+        dicts[config[line]['user']].update({start:[]})
+
+    find=False
+    for user_time in dicts[config[line]['user']].keys():
+        end_time = datetime.timestamp(datetime.fromtimestamp(float(user_time)) + timedelta(minutes=2))    
+        if float(start) <= float(end_time): 
+            dicts[config[line]['user']][user_time].append(config[line])
+            find=True
+        
+    if not find: 
+        dicts[config[line]['user']].update({start: [config[line]]})
+    
+
+for user in dicts.keys():
+    with open(user+'.json', 'w') as file:
+        file.write(json.dumps(dicts[user], sort_keys=True,
+                     indent=2, separators=(',', ': ')))
+        
